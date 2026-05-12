07a2b882d1b4e524293e54d65b842d79f91afeab
henry <aznkookiez89@yahoo.com>
2026-05-12 14:45:10 +0900

    Initialize backend setup

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using to_do_backend.Models;
+
+namespace to_do_backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController : ControllerBase
+{
+    private static List<TaskItem> tasks = new List<TaskItem>
+    {
+        new TaskItem { Id = 1, Title = "Learn .Net", Completed = false },
+        new TaskItem { Id = 2, Title = "Build API", Completed = false }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<TaskItem>> GetTasks()
+    {
+        return Ok(tasks);
+    }
+
+    [HttpPost]
+    public ActionResult<TaskItem> CreateTask(TaskItem newTask)
+    {
+        newTask.Id = tasks.Count + 1;
+        tasks.Add(newTask);
+
+        return CreatedAtAction(nameof(GetTasks), new { id = newTask.Id}, newTask);
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,8 @@
+namespace to_do_backend.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public bool Completed { get; set; }
+}
